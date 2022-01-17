d1e062e62b5a15cec346d720b22c30e7dfd03e08
felya_97@mail.ru <felya_97@mail.ru>
2022-01-17 19:17:12 +0600

    first insert to the DB(localhost PHPMyAdmin)

--- src/main/java/com/example/springbootbakcendProject/controller/EmployeeController.java
+++ src/main/java/com/example/springbootbakcendProject/controller/EmployeeController.java
@@ -1,9 +1,16 @@
 package com.example.springbootbakcendProject.controller;
 
+import com.example.springbootbakcendProject.model.Employee;
 import com.example.springbootbakcendProject.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
 
     private EmployeeService employeeService;
@@ -13,5 +20,9 @@ public class EmployeeController {
     }
 
     //build Employee Rest
-    
+    @PostMapping()
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
+        return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
 }
